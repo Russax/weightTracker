6cd93a10eecceed784edd1da9555b689d508660d
Simon Larsen <simon@larsengruppen.dk>
2019-04-26 19:14:18 +0200

    added files

---
+++ js/form.js
@@ -0,0 +1,32 @@
+formContainer = document.querySelector(".form");
+
+f_username = document.querySelector("#username");
+f_email = document.querySelector("#email");
+f_password = document.querySelector("#password");
+submitBTN = document.querySelector("#signup");
+
+class formHandler
+{
+   constructor(username, email, password)
+   {
+      this.username = username;
+      this.email = email;
+      this.password = password;
+   }
+
+   static validate()
+   {
+
+   }
+
+   static register()
+   {
+
+   }
+
+   static renderFeedback()
+   {
+
+   }
+}
+
